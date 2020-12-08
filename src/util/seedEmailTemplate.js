66646d49bbcbb95ed3f192ee11d232fb9badc938
Dinesh Bhattarai <dineshdb@dbhattarai.info.np>
2020-12-08 14:28:47 +0545

    Add ability to force update email templates via env

--- src/util/seedEmailTemplate.js
+++ src/util/seedEmailTemplate.js
@@ -5,13 +5,25 @@ import Random from "@reactioncommerce/random";
  * @param {Object} context App context
  * @param {String} shopId Shop ID
  * @param {Object} doc The template document
+ * @param {bool} force Whether to force update the doc if one exists
  * @returns {undefined}
  */
-export default async function seedEmailTemplate(context, shopId, doc) {
+export default async function seedEmailTemplate(context, shopId, doc, force = false) {
   const { Templates } = context.collections;
 
   const existing = await Templates.findOne({ language: doc.language, name: doc.name, shopId, type: "email" });
-  if (!existing) {
+  if(existing && force) {  
+    Logger.debug(`Updating database with email template "${doc.name}" for shop ${shopId}`);
+	  const template = {
+		  ...doc,
+		  _id: existing._id,
+		  parser: "handlebars",
+		  shopId,
+		  type: "email"  
+	  }
+
+	  await Templates.replaceOne({ _id: existing._id }, template, { upsert: true });
+  } else {
     Logger.debug(`Seeding database with default email template "${doc.name}" for shop ${shopId}`);
     await Templates.insertOne({
       ...doc,
@@ -22,3 +34,4 @@ export default async function seedEmailTemplate(context, shopId, doc) {
     });
   }
 }
+
--- src/util/seedEmailTemplatesForShop.js
+++ src/util/seedEmailTemplatesForShop.js
@@ -1,6 +1,7 @@
 import templateDefinitions from "../templates/index.js";
 import seedEmailTemplate from "./seedEmailTemplate.js";
 
+const forceUpdateTemplates = process.env.FORCE_UPDATE_TEMPLATES || false;
 /**
  * @summary Creates shop-owned email template records from the default templates
  *   that live in the codebase.
@@ -10,7 +11,7 @@ import seedEmailTemplate from "./seedEmailTemplate.js";
  */
 export default async function seedEmailTemplatesForShop(context, shopId) {
   const promises = templateDefinitions.map((templateDefinition) => (
-    seedEmailTemplate(context, shopId, templateDefinition)
+    seedEmailTemplate(context, shopId, templateDefinition, forceUpdateTemplates)
   ));
 
   await Promise.all(promises);
